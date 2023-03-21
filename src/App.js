175687d4295faab8e26d1f7e6c88be9f76816344
Rahul7044 <rahulkumar581999@gmail.com>
2023-03-21 17:38:17 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from './components/ExpenseItem.js'
+function App(){
+  return(
+    <div>
+      <h1>Expense Items</h1>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
