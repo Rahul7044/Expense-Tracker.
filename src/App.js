ae62975e86a69a68306b8dfbe8d313fc42b16740
Rahul7044 <rahulkumar581999@gmail.com>
2023-03-22 18:47:57 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import ExpenseItem from './components/ExpenseItem.js'
+import Expenses from './components/Expenses.js'
 function App(){
+  let expenses = [
+    {
+      id: 'e1',
+      title: 'Food',
+      amount: 10,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: 'e2',
+      title: 'Petrol',
+      amount: 100,
+      date: new Date(2021, 5, 13)
+    },
+    {
+      id: 'e3',
+      title: 'Movies',
+      amount: 200,
+      date: new Date(2021, 5, 14)
+    }
+
+  ];
   return(
     <div>
-      <h1>Expense Items</h1>
-      <ExpenseItem />
+      <h2>Let's Get Started</h2>
+
+      <Expenses item = {expenses} />
+      
+
     </div>
-  );
+  )
 }
 
 export default App;
