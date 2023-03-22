8675502bdc94006cc62bdcd8c0885c5be5b62cb0
Rahul7044 <rahulkumar581999@gmail.com>
2023-03-22 18:44:36 +0530

    create-separate-js-file-for-Dates

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,15 +1,17 @@
 import './ExpenseDate.css';
 
 function ExpenseDate(props){
-    const day = props.date.toLocaleString('en-US',{date: '2-digit'});
-    const month = props.date.toLocaleString('en-US',{month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day:'2-digit'});
+    const month = props.date.toLocaleString('en-US', {month:'long'});
     const year = props.date.getFullYear();
+
     return(
         <div className="expense-date">
-            <div className="expense-date__day">{day}</div>
-            <div className="expense-date__month">{month}</div>
-            <div className="expense-date__year">{year}</div>
-        </div>
+                <div classname="expense-date__day">{ day }</div>
+                <div classname="expense-date__month">{ month }</div>
+                <div classname="expense-date__year">{ year }</div>
+            </div>
     );
 }
+
 export default ExpenseDate;
