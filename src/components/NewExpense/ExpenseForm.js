0014ac3042dbb65b426d8ce742c972e8614ad1df
Rahul7044 <rahulkumar581999@gmail.com>
2023-03-24 10:07:21 +0530

    showing-console.log

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './ExpenseForm.css';
 
 
 const ExpenseForm = () =>{
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titlechangeHandler = (event) => {
+        console.log(event.targert.value);
+    };
+    const amountchangeHandler = (event) => {
+        console.log(event.targert.value);
+    };
+    const datechangeHandler = (event) => {
+        console.log(event.targert.value);
+    };
+
     return(
         <form>
             <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type = "text" />
+                <input type = "text"  onChange={titlechangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type = "number" min="1" step="1" />
+                <input type = "number" min="1" step="1" onChange={amountchangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type = "date" />
+                <input type = "date" onChange={datechangeHandler} />
             </div>
             </div>
             <div className="new-expense__actions">
