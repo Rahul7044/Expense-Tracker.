aff103e3c9a66315838a5cd21c6dcb7480434e05
Rahul7044 <rahulkumar581999@gmail.com>
2023-03-26 17:31:50 +0530

    filtering-function-when-no item-is-added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,20 +16,24 @@ const Expenses = (props) =>{
     const filteredExpenses = props.item.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
       });
+      let expensesContent = <p>No Expenses found</p>;
+      if(filteredExpenses.length>0){
+        expensesContent = 
+            filteredExpenses.map((expense) =>(
+                    <ExpenseItem
+                    key={expense.id}
+                       date={expense.date}
+                       title={expense.title}
+                       amount ={expense.amount} />
+                )   
+            )
+        }
+      
     return(
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {
-                filteredExpenses.map((expense) =>(
-                        <ExpenseItem
-                        key={expense.id}
-                           date={expense.date}
-                           title={expense.title}
-                           amount ={expense.amount} />
-                    )   
-                )
-            }
-        
+            
+             {expensesContent}
             
           </Card>
     );
