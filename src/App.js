bb5bd7746d0a49d18135b10beec1609a4eb89a3a
Rahul7044 <rahulkumar581999@gmail.com>
2023-03-26 14:38:20 +0530

    map-metod-used

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-const App = () => {
-  let expenses = [
-    {
-      id: 'e1',
-      title: 'Food',
-      amount: 10,
-      date: new Date(2021, 5, 12)
-    },
-    {
-      id: 'e2',
-      title: 'Petrol',
-      amount: 100,
-      date: new Date(2021, 5, 13)
-    },
-    {
-      id: 'e3',
-      title: 'Movies',
-      amount: 200,
-      date: new Date(2021, 5, 14)
-    }
+let DUMMY_EXPENSE = [
+  {
+    id: 'e1',
+    title: 'Food',
+    amount: 10,
+    date: new Date(2021, 5, 12)
+  },
+  {
+    id: 'e2',
+    title: 'Petrol',
+    amount: 100,
+    date: new Date(2021, 5, 13)
+  },
+  {
+    id: 'e3',
+    title: 'Movies',
+    amount: 200,
+    date: new Date(2021, 5, 14)
+  }
 
-  ];
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
+  
+  const addExpenseHandler = (expense) => { 
+    const updatedExpense = [expense, ...expenses];
+    setExpenses(updatedExpense);
+  }
   return(
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
 
       <Expenses item = {expenses} />
       
